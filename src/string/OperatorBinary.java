54ca19dd7cf12033db5d6cb90ed69830a08a0ff7
saipullahTI <saipullahbjraa@gmail.com>
2023-05-16 18:47:16 +0800

    Operator Binary Assignment

---
+++ src/string/OperatorBinary.java
@@ -0,0 +1,12 @@
+package string;
+
+public class OperatorBinary {
+    //operator (=) sebagai assignment/penugasan
+    //menempatatkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Saipullah";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+}
